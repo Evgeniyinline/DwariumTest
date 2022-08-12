3978e0196828f1e5515c32dcdd074e6bfca45e65
Evgeniy <longlimp@gmail.com>
2022-08-12 19:12:47 +0200

    trying to connect dwarium

--- src/test/java/dwarium/DwariumFirstTest.java
+++ src/test/java/dwarium/DwariumFirstTest.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -12,6 +13,7 @@ public class DwariumFirstTest {
     @BeforeAll
     static void configure() {
         Configuration.baseUrl = "https://w2.dwar.ru/index.php";
+        Configuration.browserBinary = "/path/to/dwarium";
         Configuration.browser = "Dwarium";
         Configuration.browserSize = "400x900";
 
@@ -19,6 +21,7 @@ public class DwariumFirstTest {
 
     @Test
     void dwariumFirstTest () {
-        open("/first_battle.php");
+        open("https://w2.dwar.ru/index.php");
+        $("#tbl-nws_link_red").shouldHave(text("ОБ ИГРЕ"));
     }
 }
